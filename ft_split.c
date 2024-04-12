c09ce016d9030e472959f2316c677319008f57a6
PedroCS98 <pedrosimoes98@hotmail.com>
2024-04-12 01:24:13 +0100

    added an initial version of split

--- ft_split.c
+++ ft_split.c
@@ -12,10 +12,55 @@
 
 #include "libft.h"
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    (void) s;
-    (void) c;
+	int		i;
+	int		j;
+	int		wc;
+	char	**array;
+	char	*str;
 
-    return(0);
+	i = -1;
+	wc = 1;
+	while (s[++i] != 0)
+	{
+		if (s[i] == c)
+			wc++;
+	}
+	array = (char **)malloc(wc + 1);
+	if (!array)
+		return(NULL);
+	i = 0;
+	wc = 0;
+	while (s[i] != 0)
+	{
+		j = 0;
+		str = (char *)malloc(ft_strlen(s) + 1);
+		if (!str)
+			return (NULL);
+		while (s[i+j++] != c)
+		{
+			str[j] = s[i + j];
+		}
+		array[wc] = str;
+		wc++;
+		free(str);
+		i += j + 1;
+	}
+	array[wc] = 0;
+	return(array);
+}
+
+int	main(){
+	char **array;
+	char *s[] = "Hoje e terca feira";
+	char c = ' ';
+	array = ft_split(s, c);
+	int i = 0;
+	while (array)
+	{
+		puts(array[i]);
+		i++;
+	}
+	free(array);
 }
