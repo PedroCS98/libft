c7abe0bb5e12e02111de29e730f4ea023e19ffc3
PedroCS98 <pedrosimoes98@hotmail.com>
2024-04-15 19:28:48 +0100

    corrected lstmap obs.: havent tested main yet

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,18 +3,20 @@
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 {
 	t_list	*new_lst;
+	t_list	*current;
 
 	if (!lst)
 		return (NULL);
 	new_lst = NULL;
 	while(lst != NULL)
 	{
-		ft_lstadd_back(new_lst, f(lst->content));
+		current = ft_lstnew(f(lst->content));
 		if (ft_lstlast(new_lst) == NULL)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
+		ft_lstadd_back(new_lst, current);
 		lst = lst -> next;	
 	}
 	//ft_lstadd_back(new_lst, NULL);
